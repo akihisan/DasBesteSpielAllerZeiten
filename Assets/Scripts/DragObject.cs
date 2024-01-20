0d8d7df268d6cde7f4c1746fdbe2c7615b1d9eca
Alexander Burkhardt <alex@de-erzgebirger.de>
2024-01-20 11:31:35 +0100

    food gets destroyed when dragged onto character

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mZCoord);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + mOffset;
+        transform.position = new Vector3(curPosition.x, curPosition.y, transform.position.z);
+    }
+
+    void OnMouseUp()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+
+        if (x >= -1 && x <= 1.8 && y >= 0 && y <= 4.5)
+        {
+        Debug.Log("collide");
+            Destroy(gameObject);
+        }
+    }
+
+
+}
