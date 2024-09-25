a16472c6f9fe8fe820b6ad065d6bc25c0a89bf54
Alexander Burkhardt <alex@de-erzgebirger.de>
2024-09-25 21:29:53 +0200

    hautfarbe kann geändert werden (noch falsche texuren)
    texture switcher script hängt an CharacterWoman > TestWoman... > Cube (nicht direkt am Prefab)

--- Assets/Scripts/CharacterCustomization/TextureSwitcher.cs
+++ Assets/Scripts/CharacterCustomization/TextureSwitcher.cs
@@ -4,35 +4,29 @@ using UnityEngine.UI;
 public class TextureSwitcher : MonoBehaviour
 {
     // Materialien mit den verschiedenen Texturen
-    public Material material1; // Die erste Textur/Material
-    public Material material2; // Die zweite Textur/Material
+    public Material darkSkin;
+    public Material lightSkin;
 
     // Das Renderer-Objekt deines Modells
     private Renderer modelRenderer;
 
-    // UI-Buttons
-    public Button button1; // Button für die erste Textur
-    public Button button2; // Button für die zweite Textur
 
     void Start()
     {
         // Renderer des Modells holen
         modelRenderer = GetComponent<Renderer>();
 
-        // Button-Listener hinzufügen
-        button1.onClick.AddListener(ApplyMaterial1);
-        button2.onClick.AddListener(ApplyMaterial2);
     }
 
     // Methode zum Wechseln auf Material 1
-    void ApplyMaterial1()
+    public void ApplyDarkSkin()
     {
-        modelRenderer.material = material1;
+        modelRenderer.material = darkSkin;
     }
 
     // Methode zum Wechseln auf Material 2
-    void ApplyMaterial2()
+    public void ApplyLightSkin()
     {
-        modelRenderer.material = material2;
+        modelRenderer.material = lightSkin;
     }
 }
